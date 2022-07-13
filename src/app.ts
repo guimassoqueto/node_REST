f240e029fb9c9a90df5f35279a514ae0a8e29917
gui.massoqueto <gui.massoqueto@gmail.com>
2022-07-13 18:06:41 -0300

    update: custom middleware for allowing CORS

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
-// middlewares imports
+// npm middlewares imports
 import bodyParser from "body-parser";
 // routes imports
 import { postsRoute } from "./routes/posts.route";
+// custom middlewares
+import allowCors from './middlewares/allowCors.middleware';
+
 // constants
 const app: Express = express();
 const PORT: number = 3000;
@@ -11,7 +14,10 @@ const PORT: number = 3000;
 app.use(bodyParser.json({
     type: 'application/json'
 }))
+// middleware cors
+app.use(allowCors)
 
+// handling routes
 app.use(postsRoute);
 
 app.listen(PORT, 'localhost', () => console.log(`Server is listening on port ${PORT}...`))
